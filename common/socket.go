352ef77cc9d642d24f3879be6f12dbf8a943be0a
spenna <sebapenna19@gmail.com>
2021-11-14 17:50:54 -0300

    Aceppt multiple clients

--- common/socket.go
+++ common/socket.go
@@ -3,13 +3,13 @@ package common
 import (
 	"bufio"
 	"fmt"
-	"io"
+	"net"
 )
 
-func Receive(c io.Reader) (string, error) {
+func Receive(c net.Conn) (string, error) {
 	return bufio.NewReader(c).ReadString('\n')
 }
 
-func Send(c io.Writer, text string) {
+func Send(c net.Conn, text string) {
 	fmt.Fprintf(c, text+"\n")
 }
--- server/server.go
+++ server/server.go
@@ -18,6 +18,25 @@ func formatPort(port string) string {
 	return ":" + port
 }
 
+func handleConnection(clientSocket net.Conn) {
+	for {
+		netData, err := common.Receive(clientSocket)
+		if err != nil {
+			logger.LogError(err)
+			return
+		}
+		if strings.TrimSpace(string(netData)) == CloseConnectionCommand {
+			fmt.Println("Exiting TCP server!")
+			clientSocket.Close()
+			return
+		}
+
+		fmt.Print("-> ", string(netData))
+		t := time.Now()
+		common.Send(clientSocket, t.Format(time.RFC3339))
+	}
+}
+
 func RunServer(port string) {
 	l, err := net.Listen(ConnectionType, formatPort(port))
 	fmt.Println("Server listening on port " + port)
@@ -27,27 +46,14 @@ func RunServer(port string) {
 	}
 	defer l.Close()
 
-	c, err := l.Accept()
-	fmt.Println("Connection accepted")
-	if err != nil {
-		logger.LogError(err)
-		return
-	}
-
 	for {
-		netData, err := common.Receive(c)
+		c, err := l.Accept()
+		fmt.Println("Connection accepted")
 		if err != nil {
 			logger.LogError(err)
 			return
 		}
-		if strings.TrimSpace(string(netData)) == CloseConnectionCommand {
-			fmt.Println("Exiting TCP server!")
-			return
-		}
-
-		fmt.Print("-> ", string(netData))
-		t := time.Now()
-		common.Send(c, t.Format(time.RFC3339))
+		go handleConnection(c)
 	}
 
 }
